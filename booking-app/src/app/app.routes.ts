24dff90a8e3f57c898b332ce5865c4acf850f4c6
RaphaelUngerFH <202548@fhwn.ac.at>
2024-12-09 19:33:59 +0100

    Add service selection page

--- booking-app/src/app/app.routes.ts
+++ booking-app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ServiceSelectionComponent } from './pages/service-selection/service-selection.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ServiceSelectionComponent }
+];
---
+++ booking-app/src/app/pages/service-selection/service-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PageComponent } from '../../components/page/page.component';
+
+@Component({
+  selector: 'app-service-selection',
+  imports: [PageComponent],
+  templateUrl: './service-selection.component.html',
+  styleUrl: './service-selection.component.css'
+})
+export class ServiceSelectionComponent {
+
+}
