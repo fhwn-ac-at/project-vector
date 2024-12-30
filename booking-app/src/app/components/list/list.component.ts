5bb7e82b161d0d23c099e949dc1cfd8837ff8f0d
RaphaelUngerFH <202548@fhwn.ac.at>
2024-12-30 17:02:29 +0100

    Add list component

---
+++ booking-app/src/app/components/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { MatListModule, MatSelectionList } from '@angular/material/list';
+
+// Component to display a list with selectable options
+@Component({
+  selector: 'app-list',
+  imports: [MatListModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  @Input() options: string[] = [];
+
+  @Output() selectionChanged = new EventEmitter<string[]>();
+
+  @ViewChild("#list") list?: MatSelectionList;
+
+  // On selection changed
+  onChange() {
+    const selected = this.list?.selectedOptions.selected.map(o => o.value);
+    this.selectionChanged.emit(selected);
+  }
+}
