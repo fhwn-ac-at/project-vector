47201760395111351ec25b0ae488659ea5718275
RaphaelUngerFH <202548@fhwn.ac.at>
2024-12-30 18:19:35 +0100

    Move list to service-selection component

--- booking-app/src/app/pages/service-selection/service-selection.component.ts
+++ booking-app/src/app/pages/service-selection/service-selection.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PageComponent } from '../../components/page/page.component';
-import { ListComponent } from "../../components/list/list.component";
+import { ApiService } from '../../services/api.service';
+import { MatListModule, MatSelectionList } from '@angular/material/list';
 
 @Component({
   selector: 'app-service-selection',
-  imports: [PageComponent, ListComponent],
+  imports: [PageComponent, MatListModule],
   templateUrl: './service-selection.component.html',
   styleUrl: './service-selection.component.css'
 })
-export class ServiceSelectionComponent {
-  services: string[] = [
-    "Haare schneiden",
-    "Haare waschen",
-    "Haare föhnen",
-    "Haare färben",
-  ];
+export class ServiceSelectionComponent implements OnInit {
+  @ViewChild("list") list?: MatSelectionList;
 
+  services: string[] = [];
   selectedServices: string[] = [];
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getServices().then((services) =>
+      this.services = services
+    );
+  }
+
+  // On selection changed emit all selected options
+  selectionChange() {
+    this.selectedServices = this.list?.selectedOptions.selected.map(o => o.value) ?? [];
+  }
 }
