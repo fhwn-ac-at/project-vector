059ae2d1df29982227f39d0a8061961f23c3f48d
RaphaelUngerFH <202548@fhwn.ac.at>
2025-01-24 14:54:37 +0100

    Add firebase config

--- booking-app/src/app/app.config.ts
+++ booking-app/src/app/app.config.ts
@@ -6,6 +6,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient } from '@angular/common/http';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
@@ -13,5 +15,13 @@ export const appConfig: ApplicationConfig = {
   provideRouter(routes), provideAnimationsAsync(), provideNativeDateAdapter(), provideServiceWorker('ngsw-worker.js', {
     enabled: !isDevMode(),
     registrationStrategy: 'registerWhenStable:30000'
-  })]
+  }),
+  provideFirebaseApp(() => initializeApp({
+    apiKey: "AIzaSyAy5yYk8kphsK0s34BUev23PV8lubiUS6k",
+    authDomain: "vector-d2098.firebaseapp.com",
+    projectId: "vector-d2098",
+    storageBucket: "vector-d2098.firebasestorage.app",
+    messagingSenderId: "1057153687079",
+    appId: "1:1057153687079:web:35a651054ee8670c9cf802"
+  }))]
 };
